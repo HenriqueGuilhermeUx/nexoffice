98d32b9b3a83a4e7a4fa7f2b300014eebd199052
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-15 19:18:08 -0300

    Surface proactive daily operations in NexOffice shell

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import OperationsDock from './OperationsDock';
+import DailyOpsPulse from './DailyOpsPulse';
 import DocWalletConnectBridge from './DocWalletConnectBridge';
 import PlatformHandoffBootstrap from './PlatformHandoffBootstrap';
 import './styles.css';
@@ -10,5 +11,5 @@ import './docwallet-connect.css';
 import './platform-handoff.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><PlatformHandoffBootstrap/><App/><OperationsDock/><DocWalletConnectBridge/></React.StrictMode>
+  <React.StrictMode><PlatformHandoffBootstrap/><App/><DailyOpsPulse/><OperationsDock/><DocWalletConnectBridge/></React.StrictMode>
 );
