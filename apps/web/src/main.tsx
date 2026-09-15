505b9f32be9ab30fbdc45c9a8dffa98b1690c79f
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-15 17:47:21 -0300

    Bootstrap one-time platform handoff in web app

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import OperationsDock from './OperationsDock';
 import DocWalletConnectBridge from './DocWalletConnectBridge';
+import PlatformHandoffBootstrap from './PlatformHandoffBootstrap';
 import './styles.css';
 import './operations-dock.css';
 import './docwallet-connect.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App/><OperationsDock/><DocWalletConnectBridge/></React.StrictMode>
+  <React.StrictMode><PlatformHandoffBootstrap/><App/><OperationsDock/><DocWalletConnectBridge/></React.StrictMode>
 );
