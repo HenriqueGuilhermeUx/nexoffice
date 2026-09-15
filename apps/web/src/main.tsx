9f059be708c04789a5afe38c4d33a143e1a4b079
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-15 20:06:42 -0300

    Mount Command Center operational radar

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import OperationsDock from './OperationsDock';
 import DailyOpsPulse from './DailyOpsPulse';
+import CommandCenterOverview from './CommandCenterOverview';
 import DocWalletConnectBridge from './DocWalletConnectBridge';
 import PlatformHandoffBootstrap from './PlatformHandoffBootstrap';
 import './styles.css';
@@ -11,5 +12,5 @@ import './docwallet-connect.css';
 import './platform-handoff.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><PlatformHandoffBootstrap/><App/><DailyOpsPulse/><OperationsDock/><DocWalletConnectBridge/></React.StrictMode>
+  <React.StrictMode><PlatformHandoffBootstrap/><App/><CommandCenterOverview/><DailyOpsPulse/><OperationsDock/><DocWalletConnectBridge/></React.StrictMode>
 );
