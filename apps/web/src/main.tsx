aee96e789732d8ed0d19e7c812dae8a83fab50f0
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-15 13:19:10 -0300

    Mount DocWallet integration connector

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import OperationsDock from './OperationsDock';
+import DocWalletConnectBridge from './DocWalletConnectBridge';
 import './styles.css';
 import './operations-dock.css';
+import './docwallet-connect.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App/><OperationsDock/></React.StrictMode>
+  <React.StrictMode><App/><OperationsDock/><DocWalletConnectBridge/></React.StrictMode>
 );
