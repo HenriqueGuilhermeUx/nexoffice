0b5cfdfcba7145e3f81c2e934002eb00a26972b1
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-15 21:18:18 -0300

    Mount standalone setup guide

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -8,11 +8,13 @@ import DigitalTeamConsole from './DigitalTeamConsole';
 import IntegrationSetupCenter from './IntegrationSetupCenter';
 import DocWalletConnectBridge from './DocWalletConnectBridge';
 import PlatformHandoffBootstrap from './PlatformHandoffBootstrap';
+import StandaloneSetup from './StandaloneSetup';
 import './styles.css';
 import './operations-dock.css';
 import './docwallet-connect.css';
 import './platform-handoff.css';
+import './standalone-setup.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><PlatformHandoffBootstrap/><App/><CommandCenterOverview/><DigitalTeamConsole/><IntegrationSetupCenter/><DailyOpsPulse/><OperationsDock/><DocWalletConnectBridge/></React.StrictMode>
+  <React.StrictMode><PlatformHandoffBootstrap/><App/><StandaloneSetup/><CommandCenterOverview/><DigitalTeamConsole/><IntegrationSetupCenter/><DailyOpsPulse/><OperationsDock/><DocWalletConnectBridge/></React.StrictMode>
 );
