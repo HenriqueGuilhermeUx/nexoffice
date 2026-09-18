bd8bf3b3c945786106b45f35204eeaa3cbd399fa
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-18 12:16:31 -0300

    Expose launch legal and support information

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -15,6 +15,7 @@ import MarketingLanding from './MarketingLanding';
 import BusinessExtensions from './BusinessExtensions';
 import StatementImportCenter from './StatementImportCenter';
 import FinancialIntelligenceCenter from './FinancialIntelligenceCenter';
+import {PublicLegalFooter,PublicLegalPage} from './PublicLegal';
 import './styles.css';
 import './operations-dock.css';
 import './docwallet-connect.css';
@@ -27,5 +28,5 @@ import './statement-import.css';
 import './financial-intelligence.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><MarketingLanding/><PlatformHandoffBootstrap/><App/><StandaloneSetup/><BillingCenter/><CommandCenterOverview/><DigitalTeamConsole/><IntegrationSetupCenter/><DocumentIntelligenceCenter/><DailyOpsPulse/><OperationsDock/><DocWalletConnectBridge/><BusinessExtensions/><StatementImportCenter/><FinancialIntelligenceCenter/></React.StrictMode>
+  <React.StrictMode><MarketingLanding/><PlatformHandoffBootstrap/><App/><StandaloneSetup/><BillingCenter/><CommandCenterOverview/><DigitalTeamConsole/><IntegrationSetupCenter/><DocumentIntelligenceCenter/><DailyOpsPulse/><OperationsDock/><DocWalletConnectBridge/><BusinessExtensions/><StatementImportCenter/><FinancialIntelligenceCenter/><PublicLegalFooter/><PublicLegalPage/></React.StrictMode>
 );
