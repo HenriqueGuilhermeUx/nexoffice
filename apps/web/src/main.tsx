52dd707fe2edc681ea90eb9b64978fc1d274e629
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-21 23:13:17 -0300

    Mount NR1Check access center

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -16,6 +16,7 @@ import StandaloneSetup from './StandaloneSetup';
 import BillingCenter from './BillingCenter';
 import MarketingLanding from './MarketingLanding';
 import BusinessExtensions from './BusinessExtensions';
+import NR1CheckAccessCenter from './NR1CheckAccessCenter';
 import StatementImportCenter from './StatementImportCenter';
 import FinancialIntelligenceCenter from './FinancialIntelligenceCenter';
 import BusinessHealthCenter from './BusinessHealthCenter';
@@ -31,6 +32,7 @@ import './standalone-setup.css';
 import './billing-center.css';
 import './marketing-landing.css';
 import './business-extensions.css';
+import './nr1check-access.css';
 import './statement-import.css';
 import './financial-intelligence.css';
 import './business-health.css';
@@ -41,5 +43,5 @@ import './intelligent-activation.css';
 import './business-trajectory.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><MarketingLanding/><PlatformHandoffBootstrap/><App/><ExecutiveBriefing/><FounderCockpit/><ActivationJourneyTracker/><StandaloneSetup/><BillingCenter/><CommandCenterOverview/><DigitalTeamConsole/><IntegrationSetupCenter/><DocumentIntelligenceCenter/><DailyOpsPulse/><OperationsDock/><DocWalletConnectBridge/><BusinessExtensions/><StatementImportCenter/><FinancialIntelligenceCenter/><BusinessHealthCenter/><BusinessRadarCenter/><BusinessKnowledgeCenter/><BusinessTrajectoryCenter/><PublicLegalFooter/><PublicLegalPage/></React.StrictMode>
+  <React.StrictMode><MarketingLanding/><PlatformHandoffBootstrap/><App/><ExecutiveBriefing/><FounderCockpit/><ActivationJourneyTracker/><StandaloneSetup/><BillingCenter/><CommandCenterOverview/><DigitalTeamConsole/><IntegrationSetupCenter/><DocumentIntelligenceCenter/><DailyOpsPulse/><OperationsDock/><DocWalletConnectBridge/><BusinessExtensions/><NR1CheckAccessCenter/><StatementImportCenter/><FinancialIntelligenceCenter/><BusinessHealthCenter/><BusinessRadarCenter/><BusinessKnowledgeCenter/><BusinessTrajectoryCenter/><PublicLegalFooter/><PublicLegalPage/></React.StrictMode>
 );
